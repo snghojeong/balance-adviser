d463622c0d6a61565258d1e60179f619260c26f2
snghojeong <snghojeong@gmail.com>
2020-02-05 20:59:03 +0900

    Implement asset as dictionary

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from pandas_datareader import data
 from matplotlib import pyplot as plt
 
+snp = data.DataReader('^GSPC','yahoo',start='1990-01-01')
+treas = data.DataReader('VFITX','yahoo',start='1990-01-01')
+
+half_bal = { 
+        "stock": { "price": 0, "amount": 0 }, 
+        "bond": { "price": 0, "amount": 0 }
+        }
+
 plt.subplot(1,2,1)
-df = data.DataReader('^GSPC','yahoo',start='1950-01-01')
-plt.plot(df['Close'], label='Close')
+plt.plot(snp['Close'], label='Close')
 plt.subplot(1,2,2)
-df = data.DataReader('VUSTX','yahoo',start='1950-01-01')
-plt.plot(df['Close'], label='Close')
+plt.plot(treas['Close'], label='Close')
 plt.show()
