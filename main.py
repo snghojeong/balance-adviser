9a98e0759d9cbc1f7e34d5fa9c5345a1fd0d47b0
snghojeong <snghojeong@gmail.com>
2020-04-11 22:06:27 +0900

    Make balance dictionary to list

--- main.py
+++ main.py
@@ -13,40 +13,41 @@ balanceOnlyStock = {
 balanceOnlyStock["stock"]["price"] = snp['Close'][0]
 balanceOnlyStock["stock"]["amount"] = math.floor(initialCash / balanceOnlyStock["stock"]["price"])
 
-balanceStockAndBond = { 
-        "stock": { "price": 0, "amount": 0, "ratio": 7 }, 
-        "bond": { "price": 0, "amount": 0, "ratio": 3 },
+balanceStockAndBond = []
+for i in range(0, 9):
+    balanceStockAndBond.append( {
+        "stock": { "price": 0, "amount": 0, "ratio": i }, 
+        "bond": { "price": 0, "amount": 0, "ratio": (10 - i) },
         "cash": { "price": 1, "amount": 0, "ratio": 0 }
-        }
-
-ratios = [v['ratio'] for k, v in balanceStockAndBond.items()]
-normRatios = ratios / np.abs(ratios).sum()
-balanceStockAndBond["stock"]["price"] = snp['Close'][0]
-balanceStockAndBond["stock"]["amount"] = math.floor((normRatios[0] * initialCash) / balanceStockAndBond["stock"]["price"])
-balanceStockAndBond["bond"]["price"] = treas['Close'][0]
-balanceStockAndBond["bond"]["amount"] = math.floor((normRatios[1] * initialCash) / balanceStockAndBond["bond"]["price"])
+        } )
+    ratios = [v['ratio'] for k, v in balanceStockAndBond[i].items()]
+    normRatios = ratios / np.abs(ratios).sum()
+    balanceStockAndBond[i]["stock"]["price"] = snp['Close'][0]
+    balanceStockAndBond[i]["stock"]["amount"] = math.floor((normRatios[0] * initialCash) / balanceStockAndBond[i]["stock"]["price"])
+    balanceStockAndBond[i]["bond"]["price"] = treas['Close'][0]
+    balanceStockAndBond[i]["bond"]["amount"] = math.floor((normRatios[1] * initialCash) / balanceStockAndBond[i]["bond"]["price"])
 
 onlyStock = [ (balanceOnlyStock["stock"]["price"] * balanceOnlyStock["stock"]["amount"]) ]
 
-portfolio = [ (balanceStockAndBond["stock"]["price"] * balanceStockAndBond["stock"]["amount"]) + 
-              (balanceStockAndBond["bond"]["price"] * balanceStockAndBond["bond"]["amount"]) ]
+portfolio = [ (balanceStockAndBond[5]["stock"]["price"] * balanceStockAndBond[5]["stock"]["amount"]) + 
+              (balanceStockAndBond[5]["bond"]["price"] * balanceStockAndBond[5]["bond"]["amount"]) ]
 
-stock = [balanceStockAndBond["stock"]["amount"]]
-bond = [balanceStockAndBond["bond"]["amount"]]
-cash = [balanceStockAndBond["cash"]["amount"]]
+stock = [balanceStockAndBond[5]["stock"]["amount"]]
+bond = [balanceStockAndBond[5]["bond"]["amount"]]
+cash = [balanceStockAndBond[5]["cash"]["amount"]]
 
 for s, b in zip(snp['Close'], treas['Close']):
-    balanceStockAndBond['stock']['price'] = s
-    balanceStockAndBond['bond']['price'] = b
-    rebalance(balanceStockAndBond)
-    stockVal = s * balanceStockAndBond["stock"]["amount"]
-    bondVal = b * balanceStockAndBond["bond"]["amount"]
-    cashVal = balanceStockAndBond["cash"]["amount"]
+    balanceStockAndBond[5]['stock']['price'] = s
+    balanceStockAndBond[5]['bond']['price'] = b
+    rebalance(balanceStockAndBond[5])
+    stockVal = s * balanceStockAndBond[5]["stock"]["amount"]
+    bondVal = b * balanceStockAndBond[5]["bond"]["amount"]
+    cashVal = balanceStockAndBond[5]["cash"]["amount"]
     portfolio.append(stockVal + bondVal + cashVal)
     onlyStock.append(s * balanceOnlyStock["stock"]["amount"])
-    stock.append(balanceStockAndBond["stock"]["amount"])
-    bond.append(balanceStockAndBond["bond"]["amount"])
-    cash.append(balanceStockAndBond["cash"]["amount"])
+    stock.append(balanceStockAndBond[5]["stock"]["amount"])
+    bond.append(balanceStockAndBond[5]["bond"]["amount"])
+    cash.append(balanceStockAndBond[5]["cash"]["amount"])
 
 print(snp)
 print(treas)
