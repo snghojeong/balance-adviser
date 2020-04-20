c4ffc7fd9fc87fe13475f3203655725a24aca878
snghojeong <snghojeong@gmail.com>
2020-04-20 20:14:54 +0900

    Modify balance variable to represent many ratio

--- main.py
+++ main.py
@@ -16,19 +16,18 @@ balanceOnlyStock["stock"]["amount"] = math.floor(initialCash / balanceOnlyStock[
 balanceStockAndBond = []
 for i in range(0, 9):
     balanceStockAndBond.append( {
-        "stock": { "price": 0, "amount": 0, "ratio": i }, 
         "bond": { "price": 0, "amount": 0, "ratio": (10 - i) },
-        "cash": { "price": 1, "amount": 0, "ratio": 0 }
+        "cash": { "price": 1, "amount": 0, "ratio": 0 }, 
+        "stock": { "price": 0, "amount": 0, "ratio": i }
         } )
-    ratios = [v['ratio'] for k, v in balanceStockAndBond[i].items()]
+    ratios = [float(v['ratio']) for k, v in balanceStockAndBond[i].items()]
     normRatios = ratios / np.abs(ratios).sum()
     balanceStockAndBond[i]["stock"]["price"] = snp['Close'][0]
-    balanceStockAndBond[i]["stock"]["amount"] = math.floor((normRatios[0] * initialCash) / balanceStockAndBond[i]["stock"]["price"])
+    balanceStockAndBond[i]["stock"]["amount"] = math.floor((normRatios[2] * initialCash) / balanceStockAndBond[i]["stock"]["price"])
     balanceStockAndBond[i]["bond"]["price"] = treas['Close'][0]
-    balanceStockAndBond[i]["bond"]["amount"] = math.floor((normRatios[1] * initialCash) / balanceStockAndBond[i]["bond"]["price"])
+    balanceStockAndBond[i]["bond"]["amount"] = math.floor((normRatios[0] * initialCash) / balanceStockAndBond[i]["bond"]["price"])
 
 onlyStock = [ (balanceOnlyStock["stock"]["price"] * balanceOnlyStock["stock"]["amount"]) ]
-
 portfolio = [ (balanceStockAndBond[5]["stock"]["price"] * balanceStockAndBond[5]["stock"]["amount"]) + 
               (balanceStockAndBond[5]["bond"]["price"] * balanceStockAndBond[5]["bond"]["amount"]) ]
 
@@ -37,13 +36,15 @@ bond = [balanceStockAndBond[5]["bond"]["amount"]]
 cash = [balanceStockAndBond[5]["cash"]["amount"]]
 
 for s, b in zip(snp['Close'], treas['Close']):
-    balanceStockAndBond[5]['stock']['price'] = s
-    balanceStockAndBond[5]['bond']['price'] = b
-    rebalance(balanceStockAndBond[5])
-    stockVal = s * balanceStockAndBond[5]["stock"]["amount"]
-    bondVal = b * balanceStockAndBond[5]["bond"]["amount"]
-    cashVal = balanceStockAndBond[5]["cash"]["amount"]
-    portfolio.append(stockVal + bondVal + cashVal)
+    for i in range(0, 9):
+        balanceStockAndBond[i]['stock']['price'] = s
+        balanceStockAndBond[i]['bond']['price'] = b
+        rebalance(balanceStockAndBond[i])
+        stockVal = s * balanceStockAndBond[i]["stock"]["amount"]
+        bondVal = b * balanceStockAndBond[i]["bond"]["amount"]
+        cashVal = balanceStockAndBond[i]["cash"]["amount"]
+        if i == 5:
+            portfolio.append(stockVal + bondVal + cashVal)
     onlyStock.append(s * balanceOnlyStock["stock"]["amount"])
     stock.append(balanceStockAndBond[5]["stock"]["amount"])
     bond.append(balanceStockAndBond[5]["bond"]["amount"])
