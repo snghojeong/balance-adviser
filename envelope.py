af5b0bcc469d4a4fdac09c91c2efc220f44f5021
snghojeong <snghojeong@gmail.com>
2020-06-20 14:08:35 +0900

    Modify envelope low bound

--- envelope.py
+++ envelope.py
@@ -2,4 +2,4 @@ def envelopeHiBounds(valueList, wnd, ratio):
     return valueList.ewm(50).mean() * (1 + ratio)
 
 def envelopeLoBounds(valueList, wnd, ratio):
-    return valueList.ewm(50).mean() * (1 - ratio)
+    return valueList.ewm(50).mean()
