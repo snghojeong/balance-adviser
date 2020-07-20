537aff4fac8a26f2f91b8aed4633bd000927f3c1
snghojeong <snghojeong@gmail.com>
2020-07-20 15:40:40 +0900

    Modify lo bound to rearrange ratio more aggresively

--- envelope.py
+++ envelope.py
@@ -2,4 +2,4 @@ def envelopeHiBounds(valueList, wnd, ratio):
     return valueList.ewm(50).mean() * (1 + ratio)
 
 def envelopeLoBounds(valueList, wnd, ratio):
-    return valueList.ewm(50).mean()
+    return valueList.ewm(50).mean() * (1 + ratio)
--- main.py
+++ main.py
@@ -73,7 +73,7 @@ for i in range(0, 9):
 dynamicPortfolio.append((balanceStockAndBond[i]["stock"]["price"] * balanceStockAndBond[i]["stock"]["amount"]) + 
             (balanceStockAndBond[i]["bond"]["price"] * balanceStockAndBond[i]["bond"]["amount"]))
 
-for s, b, hi, lo in zip(snp['Close'], treas['Close'], envelopeHiBounds(snp['Close'], 50, 0.1), envelopeLoBounds(snp['Close'], 50, 0.1)):
+for s, b, hi, lo in zip(snp['Close'], treas['Close'], envelopeHiBounds(snp['Close'], 50, 0.1), envelopeLoBounds(snp['Close'], 50, 0.05)):
     for i in range(0, 9):
         balanceStockAndBond[i]['stock']['price'] = s
         balanceStockAndBond[i]['bond']['price'] = b
@@ -105,7 +105,7 @@ plt.subplot(3,1,2)
 plt.plot(snp['Close'], label='Stock')
 plt.plot(snp['Close'].ewm(50).mean(), label='EMA')
 plt.plot(envelopeHiBounds(snp['Close'], 50, 0.1), label='upper')
-plt.plot(envelopeLoBounds(snp['Close'], 50, 0.1), label='lower')
+plt.plot(envelopeLoBounds(snp['Close'], 50, 0.05), label='lower')
 plt.legend(loc='upper left')
 plt.subplot(3,1,3)
 plt.plot(ratio, label='Stock ratio')
