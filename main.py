5e081724a8c3dfe530e3e122f6975c53454c2d3e
snghojeong <snghojeong@gmail.com>
2020-07-30 18:34:55 +0900

    Modify to get last date as key

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class StaticPortfolio:
             balance[item["name"]] = { "price": price, 
                                       "amount": amount, 
                                       "ratio": item["ratio"] }
-        date_range = pd.period_range(start='2003-01-02', end='2020-07-27', freq='D')
+        date_range = pd.period_range(start='2003-01-02', end=snp['Close'].keys()[-1], freq='D')
         for d in date_range.astype(str):
             if d in snp['Close']:
                 print(snp['Close'][d])
