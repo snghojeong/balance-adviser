45f1282548629008550a5dc18adf0e528c7e3f16
snghojeong <snghojeong@gmail.com>
2020-08-30 12:26:51 +0900

    Modify envelop ratio for dynamic portfolio

--- envelope.py
+++ envelope.py
@@ -2,7 +2,7 @@ def envelopeHiBounds(valueList, wnd):
     return envelopeBounds(valueList, wnd, 0.05)
 
 def envelopeLoBounds(valueList, wnd):
-    return envelopeBounds(valueList, wnd, 0.05)
+    return envelopeBounds(valueList, wnd, 0.025)
 
 def envelopeBounds(valueList, wnd, ratio):
     return valueList.ewm(wnd).mean() * (1 + ratio)
