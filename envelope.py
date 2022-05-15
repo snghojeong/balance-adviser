cd07304b262ff59315b9d20862642ec8646faea7
Seungho Jeong <snghojeong@gmail.com>
2022-05-15 22:07:42 +0900

    Update envelope.py

--- envelope.py
+++ envelope.py
@@ -1,8 +1,8 @@
 def envelopeHiBounds(valueList, wnd):
-    return envelopeBounds(valueList, wnd, 0.025)
+    return envelopeBounds(valueList, wnd, 0.05)
 
 def envelopeLoBounds(valueList, wnd):
-    return envelopeBounds(valueList, wnd, 0.025)
+    return envelopeBounds(valueList, wnd, 0.05)
 
 def envelopeBounds(valueList, wnd, ratio):
     return valueList.ewm(wnd).mean() * (1 + ratio)
