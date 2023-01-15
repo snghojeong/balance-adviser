0b3681978f4ac0109246fd4a0b5b7d91493b9700
Seungho Jeong <snghojeong@gmail.com>
2023-01-15 13:11:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,66 @@ test2 = bt.Backtest(s2, data)
 # we include test here to see the results side-by-side
 res2 = bt.run(test, test2)
 
-res2.plot();
+res2.plot()
 res2.display()
+
+# download data
+data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
+
+# calculate moving average DataFrame using pandas' rolling_mean
+import pandas as pd
+# a rolling mean is a moving average, right?
+sma = data.rolling(50).mean()
+
+plot = bt.merge(data, sma).plot(figsize=(15, 5))
+
+class SelectWhere(bt.Algo):
+
+    """
+    Selects securities based on an indicator DataFrame.
+
+    Selects securities where the value is True on the current date (target.now).
+
+    Args:
+        * signal (DataFrame): DataFrame containing the signal (boolean DataFrame)
+
+    Sets:
+        * selected
+
+    """
+    def __init__(self, signal):
+        self.signal = signal
+
+    def __call__(self, target):
+        # get signal on target.now
+        if target.now in self.signal.index:
+            sig = self.signal.loc[target.now]
+
+            # get indices where true as list
+            selected = list(sig.index[sig])
+
+            # save in temp - this will be used by the weighing algo
+            target.temp['selected'] = selected
+
+        # return True because we want to keep on moving down the stack
+        return True
+
+# first we create the Strategy
+s = bt.Strategy('above50sma', [SelectWhere(data > sma),
+                               bt.algos.WeighEqually(),
+                               bt.algos.Rebalance()])
+
+# now we create the Backtest
+t = bt.Backtest(s, data)
+
+# and let's run it!
+res = bt.run(t)
+
+# what does the equity curve look like?
+res.plot()
+
+# and some performance stats
+res.display()
 
 # start day of TLT: 2003-01-02
 snp = data.DataReader('^GSPC', 'yahoo', start='2003-01-02')
