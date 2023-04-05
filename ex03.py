00bd63491a2ea0e43b135feb0e84aadd1bb84c2b
seungho.jung <seungho.jung@linecorp.com>
2023-04-05 22:39:19 +0900

    Add ex03

---
+++ ex03.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import ffn
+import bt
+
+%matplotlib inline
+
+names = ['foo','bar','rf']
+dates = pd.date_range(start='2015-01-01',end='2018-12-31', freq=pd.tseries.offsets.BDay())
+n = len(dates)
+rdf = pd.DataFrame(
+    np.zeros((n, len(names))),
+    index = dates,
+    columns = names
+)
+
+np.random.seed(1)
+rdf['foo'] = np.random.normal(loc = 0.1/252,scale=0.2/np.sqrt(252),size=n)
+rdf['bar'] = np.random.normal(loc = 0.04/252,scale=0.05/np.sqrt(252),size=n)
+rdf['rf'] = 0.
+
+pdf = 100*np.cumprod(1+rdf)
+pdf.plot();
