c6afe675bfd88228981cba3fb197819df08be256
Seungho Jeong <snghojeong@gmail.com>
2023-04-06 18:41:40 +0900

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -23,3 +23,35 @@ rdf['rf'] = 0.
 
 pdf = 100*np.cumprod(1+rdf)
 pdf.plot();
+
+selectTheseAlgo = bt.algos.SelectThese(['foo','bar'])
+
+# algo to set the weights to 1/vol contributions from each asset
+#  with data over the last 3 months excluding yesterday
+weighInvVolAlgo = bt.algos.WeighInvVol(
+    lookback=pd.DateOffset(months=3),
+    lag=pd.DateOffset(days=1)
+)
+
+# algo to rebalance the current weights to weights set in target.temp
+rebalAlgo = bt.algos.Rebalance()
+
+# a strategy that rebalances daily to 1/vol weights
+strat = bt.Strategy(
+    'Target',
+    [
+        selectTheseAlgo,
+        weighInvVolAlgo,
+        rebalAlgo
+    ]
+)
+
+# set integer_positions=False when positions are not required to be integers(round numbers)
+backtest = bt.Backtest(
+    strat,
+    pdf,
+    integer_positions=False
+)
+
+res_target = bt.run(backtest)
+res_target.get_security_weights().plot();
